b4528c838e2422aad40b6c5830aae1805f1aadeb
bhavyachappa12-ui <bhavyachappa12@gmail.com>
2025-09-19 16:41:52 +0530

    Created SignUp page

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import NavBar from "./NavBar" 
+import SignUp from "./Signup"
+function App() {
+
+  return (
+    <div>
+      <NavBar/>
+      <SignUp/>
+
+    </div>  )
+}
+
+export default App
---
+++ frontend/src/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const NavBar = () => {
+    return(
+        <div className="flex justify-around h-16 items-center border-2 border-black">
+            <h1 className="text-xl font-bold">BlogVerse</h1>
+            <button className="text-gray-700 hover:bg-blue-200 px-5 py-2 rounded-lg cursor-pointer">Home</button>
+            <div className="flex gap-5">
+                <button className="font-semibold text-gray-700 cursor-pointer">Sign In</button>
+                <button className="bg-blue-600 text-white px-5 py-2 rounded-xl cursor-pointer">Sign up</button>
+            </div>
+
+        </div>
+    )}
+    export default NavBar
