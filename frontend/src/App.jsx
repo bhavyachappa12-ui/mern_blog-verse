05d74826766e8e8f635e45cd963ce0858773b7e4
bhavyachappa12-ui <bhavyachappa12@gmail.com>
2025-09-20 09:31:10 +0530

    Created SignIn page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
 
 import NavBar from "./NavBar" 
 import SignUp from "./Signup"
+import SignIn from "./SignIn"
 function App() {
 
   return (
     <div>
       <NavBar/>
-      <SignUp/>
+      
+      <SignIn/>
+      
 
     </div>  )
 }
--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -6,7 +6,7 @@ const NavBar = () => {
             <button className="text-gray-700 hover:bg-blue-200 px-5 py-2 rounded-lg cursor-pointer">Home</button>
             <div className="flex gap-5">
                 <button className="font-semibold text-gray-700 cursor-pointer">Sign In</button>
-                <button className="bg-blue-600 text-white px-5 py-2 rounded-xl cursor-pointer">Sign up</button>
+                <button className="bg-blue-600 text-white px-5 py-2 rounded-xl cursor-pointer">Sign Up</button>
             </div>
 
         </div>
